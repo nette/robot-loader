435f78b4734ad7152806d75242d3d8898aecf34f
David Grudl <david@grudl.com>
2021-03-01 19:56:54 +0100

    added PHP 8 typehints

--- src/RobotLoader/RobotLoader.php
+++ src/RobotLoader/RobotLoader.php
@@ -83,7 +83,7 @@ class RobotLoader
 	/**
 	 * Register autoloader.
 	 */
-	public function register(bool $prepend = false): self
+	public function register(bool $prepend = false): static
 	{
 		spl_autoload_register([$this, 'tryLoad'], true, $prepend);
 		return $this;
@@ -134,16 +134,15 @@ class RobotLoader
 
 	/**
 	 * Add path or paths to list.
-	 * @param  string  ...$paths  absolute path
 	 */
-	public function addDirectory(...$paths): self
+	public function addDirectory(string ...$paths): static
 	{
 		$this->scanPaths = array_merge($this->scanPaths, $paths);
 		return $this;
 	}
 
 
-	public function reportParseErrors(bool $on = true): self
+	public function reportParseErrors(bool $on = true): static
 	{
 		$this->reportParseErrors = $on;
 		return $this;
@@ -152,9 +151,8 @@ class RobotLoader
 
 	/**
 	 * Excludes path or paths from list.
-	 * @param  string  ...$paths  absolute path
 	 */
-	public function excludeDirectory(...$paths): self
+	public function excludeDirectory(string ...$paths): static
 	{
 		$this->excludeDirs = array_merge($this->excludeDirs, $paths);
 		return $this;
@@ -404,7 +402,7 @@ class RobotLoader
 	/**
 	 * Sets auto-refresh mode.
 	 */
-	public function setAutoRefresh(bool $on = true): self
+	public function setAutoRefresh(bool $on = true): static
 	{
 		$this->autoRebuild = $on;
 		return $this;
@@ -414,7 +412,7 @@ class RobotLoader
 	/**
 	 * Sets path to temporary directory.
 	 */
-	public function setTempDirectory(string $dir): self
+	public function setTempDirectory(string $dir): static
 	{
 		Nette\Utils\FileSystem::createDir($dir);
 		$this->tempDirectory = $dir;
